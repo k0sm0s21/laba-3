4414606f08abf3b3de28ac0d4f4de1f1467adc91
k0sm0s21 <145329561+k0sm0s21@users.noreply.github.com>
2023-10-25 13:20:21 +0300

    Add files via upload

---
+++ 3.3/3.3/3.3.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	{
+		setlocale(0, "");
+		const int a = 10;
+		long long b = 1;
+		for (int i = 1; i <= a; i++)
+			b = b * (i * (i + 1)) / 2;
+		cout <<"Получилось число: "<< b << endl;
+		
+	}
+	return 0;
+}
---
+++ 3.4/3.4/3.4.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	setlocale(0, "");
+	int number;
+	int count = 0;
+	int sum = 0;
+
+	cout << "Введите числа (0 для выхода):" << endl;
+	while (true) {
+		cin >> number;
+
+		if (number == 0) {
+			break;
+		}
+
+		count++;
+		sum += number;
+	}
+
+	if (count > 0) {
+		double average = static_cast<double>(sum) / count;
+		cout << "Количество чисел: " << count << endl;
+		cout << "Сумма чисел: " << sum << endl;
+		cout << "Среднее арифметическое: " << average << endl;
+	}
+	else {
+		cout << "Ни одно число не было введено!" << endl;
+	}
+
+	return 0;
+}
