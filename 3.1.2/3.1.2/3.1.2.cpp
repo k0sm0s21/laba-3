3dd9443a83e6438234381146ed45e582034927c9
k0sm0s21 <145329561+k0sm0s21@users.noreply.github.com>
2023-10-25 15:00:44 +0300

    Add files via upload

---
+++ 3.1.2/3.1.2/3.1.2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    int negative_index = -1;
+    for (int i = 0; i < n; i++) {
+        if (a[i] < 0) {
+            negative_index = i;
+            break;
+        }
+    }
+    for (int i = 0; i < negative_index; i++) {
+        cout << a[i] << " ";
+    }
+    for (int i = negative_index; i < n; i++) {
+        cout << a[i] << " ";
+    }
+
+    return 0;
+}
