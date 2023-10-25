7f8321c5a17daba698553a76f6ff62fb2029275e
k0sm0s21 <145329561+k0sm0s21@users.noreply.github.com>
2023-10-25 13:19:21 +0300

    Add files via upload

---
+++ 3.2/3.2/3.2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(0, "");
+	int let;
+	float proc, i, summ;
+	cout << "Сумма вклада: ";
+	cin >> summ;
+	cout << "На сколько лет: ";
+	cin >> let;
+	proc = summ * 3 / 100;
+	i = summ + proc * let;
+	cout << "Сумма через " << let << " лет: " << i << endl;
+	system("PAUSE");
+	return 0;
+}
